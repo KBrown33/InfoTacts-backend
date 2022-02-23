9678c37c473eac723cceeb7ebbfae891aaebb354
Kevin <kbrown2derlon@gmail.com>
2022-02-23 15:02:53 -0600

    first commit

---
+++ src/main/java/com/example/picgramnew/PicgramNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.picgramnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicgramNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicgramNewApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/picgramnew/model/User.java
@@ -0,0 +1,4 @@
+package com.example.picgramnew.model;
+
+public class User {
+}
