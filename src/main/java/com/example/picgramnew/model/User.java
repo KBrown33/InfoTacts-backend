c42702a3d6c016bd1a71219feece96e9707bbf50
Kevin <kbrown2derlon@gmail.com>
2022-02-24 17:04:34 -0600

    one model new update to InfoTacts 2

--- src/main/java/com/example/picgramnew/model/User.java
+++ src/main/java/com/example/picgramnew/model/User.java
@@ -12,11 +12,57 @@ public class User {
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column
     private String name;
-    @Column
+
+    @Column(unique = true)
     private String email;
 
     @Column
     private String userGroup;
+    @Column
+    private String address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserGroup() {
+        return userGroup;
+    }
+
+    public void setUserGroup(String userGroup) {
+        this.userGroup = userGroup;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
 }
